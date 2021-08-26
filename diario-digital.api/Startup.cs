88dd76cdc2446c976740ac0be486e61c5633d263
Tomas Campos <tomasantoniocampos@gmail.com>
2021-08-26 18:11:19 -0300

    Configurando Swagger

--- diario-digital.api/Startup.cs
+++ diario-digital.api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,16 @@ namespace diario_digital.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Diário Digital",
+                    Description = "API",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +46,14 @@ namespace diario_digital.api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(swagger =>
+            {
+                swagger.SwaggerEndpoint("/swagger/v1/swagger.json", "Diário Digital");
+                swagger.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
